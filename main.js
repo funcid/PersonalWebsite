a391a73897fff688da273a273853211680c3c193
funcid <rtem903134@gmail.com>
2020-02-22 02:58:23 +0300

    Анимация печати и адаптивность под телефоны

--- main.js
+++ main.js
@@ -15,16 +15,27 @@ window.onload = function() {
     });
     var angle = 0;
     var age = document.getElementById("age");
+    var radius = 500;
+    var dHeight = 0;
+    if (this.width < 550) {
+        radius = 100;
+        dHeight = this.height/3;
+    }
+    ['f|__', 'f___', 'fu__', 'fu|_', 'fu|_', 'fun_', 'fun|', 'fun_', 'func', 'func|', 'func'].forEach(function(str, i, arr) {
+        this.setTimeout(() => {
+            center.innerHTML = str;
+        }, 200 * i);
+    });
     setInterval(() => {
         age.innerHTML = ((new Date().getTime()-1058832000000)/3600000/24/365).toFixed(10) + " лет";
         if (!this.mouseOver) {
             angle = angle > 100000000 ? 0 : angle + 1;
             logos.forEach(function(logo, i, arr) {
-                logo.style.left = width - center.clientWidth - logo.clientWidth/2 + Math.cos(angle/100 + i*2) * 500 + 'px';
-                logo.style.top = height - center.clientHeight - logo.height/2 + Math.sin(angle/100 + i*2) * 500 + 'px';
+                logo.style.left = width - center.clientWidth - logo.clientWidth/2 + Math.cos(angle/99 + (i + 1) * 2) * radius + 'px';
+                logo.style.top = height - center.clientHeight + dHeight - logo.height/2 + Math.sin(angle/99 + (i + 1) * 2) * radius + 'px';
             });
         } 
-    }, 1);
+    }, 5);
 };
 
 function changeLogo(logo, opacity, border, boxshadow) {
